9b5a41a3d4ff8681d06007aa705e9a13c47b4c27
mauricio9917 <ardila.mauricio@yahoo.com>
2021-09-23 23:04:35 -0500

    hello partners

--- form.js
+++ form.js
@@ -1,30 +1,59 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("form-registro").addEventListener('submit', checkDir); //es la manera de decir que esta funcion checkDir es para el formulario de id=form-registro
-    // para agregar otra funcion a este documento debe repetir la linea de ariba pero cambiando el nombre de la funcion
-    document.getElementById("form-registro").addEventListener('submit', checkGen); //verifica la funcion chekGen osea verifica que tenga un genero seleccionado
-  });  
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("form-registro").addEventListener("submit", checkDir); //es la manera de decir que esta funcion checkDir es para el formulario de id=form-registro
+  // para agregar otra funcion a este documento debe repetir la linea de ariba pero cambiando el nombre de la funcion
+  document.getElementById("form-registro").addEventListener("submit", checkGen); //verifica la funcion chekGen osea verifica que tenga un genero seleccionado
+});
 
-  direccionCorrect=/^[a-zA-Z0-9\#\-]{1,50}$/; //es el parametro que me permite decir que acepta letras y numeros y los caracteres especiales #- y de 1 a 50 cantidad
-  
-  function checkDir(valor) { //mi Funcion
-    valor.preventDefault();
-    var direccion=document.getElementById('direccion').value; //captura el valor del input del resgistro.html
-    if(direccionCorrect.test(direccion)){ //hace la comparacion que direccion cumpla con los parametros de direccionCorrect
-      return true;
-    }else{
-    
-      alert('Direccion incorrecta');
-      return false;
-    }
+direccionCorrect = /^[a-zA-Z0-9\#\-]{1,50}$/; //es el parametro que me permite decir que acepta letras y numeros y los caracteres especiales #- y de 1 a 50 cantidad
+correo = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+telefono = /^\d{7,15}$/; // 7 a 15 numeros.
+
+function checkTelefono(valor){
+  valor.preventDefault();
+  var telefono = document.getElementById("telefono").value;
+  if(telefonoCorrect.test(telefono)){
+    alert("Telefono correcto");
+    return true;
+  } else {
+    alert("Telefono no válido");
+    return false;
+  }
 }
-//module.exports=checkDir; lo puse asi por q tira error aun no se por q
 
-function checkGen(valor){
-    valor.preventDefault();
-  if(!document.querySelector('input[name="color"]:checked')==true) {//si hay algun selector activo arroja false
+function checkDir(valor){
+  //mi Funcion
+  valor.preventDefault();
+  var direccion = document.getElementById("direccion").value; //captura el valor del input del resgistro.html
+  if (direccionCorrect.test(direccion)) {
+    //hace la comparacion que direccion cumpla con los parametros de direccionCorrect
+    return true;
+  } else {
+    alert("Direccion incorrecta");
+    return false;
+  }
+}
+//module.exports=checkDir; //lo puse asi por q tira error aun no se por q
+//no sé la manera de arreglar ese error.
+
+function checkGen(valor) {
+  valor.preventDefault();
+  if (!document.querySelector('input[name="color"]:checked') == true) {
+    //si hay algun selector activo arroja false
     alert("Seleccione por favor un genero");
-      return false;
-    }else{
-        return true;
+    return false;
+  } else {
+    return true;
+  }
 }
 
+function checkCorreo(valor){
+  valor.preventDefault();
+  var correo = document.getElementById("correo").value;
+  if(correoCorrect.test(correo)){
+    alert("Correo correcto");
+    return true;
+  } else {
+    alert("Correo incorrecto");
+    return false;
+  }
+} 
