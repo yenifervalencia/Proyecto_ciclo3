4295e80ec96056d7bd1b3dd152c74ed18882b3bd
Miguel Trujillo <mats481@gmail.com>
2021-09-23 19:14:00 -0500

    verificar genero

--- form.js
+++ form.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("form-registro").addEventListener('submit', checkDir); //es la manera de decir que esta funcion checkDir es para el formulario de id=form-registro
     // para agregar otra funcion a este documento debe repetir la linea de ariba pero cambiando el nombre de la funcion
+    document.getElementById("form-registro").addEventListener('submit', checkGen); //verifica la funcion chekGen osea verifica que tenga un genero seleccionado
   });  
 
   direccionCorrect=/^[a-zA-Z0-9\#\-]{1,50}$/; //es el parametro que me permite decir que acepta letras y numeros y los caracteres especiales #- y de 1 a 50 cantidad
@@ -18,4 +19,10 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 }
 //module.exports=checkDir; lo puse asi por q tira error aun no se por q
+
+function checkGen(){
+  if(!document.querySelector('input[name="color"]:checked')==true) {//si hay algun selector activo arroja false
+    alert("Seleccione por favor un genero");
+    }
+}
 
