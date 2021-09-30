d2105187a4a3bc7a1ca66b6b10b2c14ebc725740
ValeriaZapataCeballos <valerizapata234@gmail.com>
2021-09-30 14:07:09 -0500

    Array

--- registro.js
+++ registro.js
@@ -1,26 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("form-registro").addEventListener('submit',filtrarCorreo)
-});
+});*/
 
-function agregarRegistro(){
-	function capturar(){
-		function Persona(nombre) {
-			this.nombre = nombre;
-		}
-		var nombreCapturar = document.getElementById("nombre").value;
-		nuevoCliente = new Persona(nombreCapturar);
-		console.log(nuevoCliente);
-		agregar();
-	}
-	var arreglo = [];
-	function agregar(){
-		arreglo.push(nuevoCliente);
-		console.log(arreglo);
-	}
+function conseguirDatos(){
+    function Persona(nombre,sexo,telefono,direccion,correo,contrasena){
+        this.nombre=nombre;
+		this.sexo=sexo;
+        this.telefono=telefono;
+        this.direccion=direccion;
+        this.correo=correo;
+        this.contrasena=contrasena;
+    }
+     datoNombre = document.getElementById('nombre').value;
+     datoGenero = document.querySelector('input[name="sexo"]:checked').value;
+     datoTelefono = document.getElementById('telefono').value;
+    datoDireccion=document.getElementById('direccion').value;
+    datoCorreo = document.getElementById('correo').value;
+    datoContrasena = document.getElementById('contrasena').value;
+    nuevoCliente = new Persona(datoNombre,datoGenero,datoTelefono,datoDireccion,datoCorreo,datoContrasena);
+
+	console.log(nuevoCliente);
+    agregarRegistro();
 }
-
-
+var arreglo = [];
+function agregarRegistro(){
+	arreglo.push(nuevoCliente);
+	console.log(arreglo);
+}
+/*
 function filtrarCorreo(){
     let filtro=arregloo.filter((arregloo) => arregloo.correo.includes("@gmail.com"));
     console.log(filtro);    
-}
+}*/
