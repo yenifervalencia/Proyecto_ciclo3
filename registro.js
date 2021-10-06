5d3a60d7fa7b68e73614a41a4c2be3db61942ac9
yenifervalencia <90335928+yenifervalencia@users.noreply.github.com>
2021-10-06 16:45:40 -0500

    Update registro.js

--- registro.js
+++ registro.js
@@ -1,47 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("form-registro").addEventListener('submit',filtrarCorreo)
 });
+//comentar lo de arriba
 
 
 let registros = []; 
 
 function agregarRegistro(){
-    //console.log("Capturado");
-    function Persona(
-      nombreP,
-     
-      telefonoP,
-      direccionP,
-      correoP,
-      contrasenaP
-    ) {
-      this.nombreP = nombreP;
-      
-      this.telefonoP = telefonoP;
-      this.direccionP = direccionP;
-      this.correoP = correoP;
-      this.contrasenaP = contrasenaP;
-    }
-    var nombre= document.getElementById("nombre").value;
-    //console.log(nombreCapturar);
    
-    //console.log(generoCapturar);
-    var telefono = document.getElementById("telefono").value;
-    //console.log(telefonoCapturar);
-    var direccion = document.getElementById("direccion").value;
-    //console.log(direccionCapturar);
-    var correo = document.getElementById("correo").value;
-    //console.log(correoCapturar);
-    var contrasena = document.getElementById("contrasena").value;
-    //console.log(contrasenaCapturar);
-    nuevoCliente = new Persona(
-      nombre,    
-      telefono,
-      direccion,
-      correo,
-      contrasena
-    );
-    //console.log(nuevoCliente);
+   let nombreUsu= document.getElementById("nombre").value;
+    let telefonoUsu = document.getElementById("telefono").value;
+    let direccionUsu = document.getElementById("direccion").value;
+    let correoUsu = document.getElementById("correo").value;
+    let contrasenaUsu = document.getElementById("contrasena").value;
+
+    let nuevoCliente = {
+      "nombre": nombreUsu,
+        //    "genero": generoUsu,
+        "telefono": telefonoUsu,
+        "direccion": direccionUsu,
+        "correo": correoUsu,
+        "password": contrasenaUsu
+    }
    
   
   
@@ -62,11 +42,13 @@ function filtrarCorreo(registros){
 }
 
 function ordenarArreglo(arreglo){
-  arreglo.sort(function (a, b) {
-    if (a.Nombre > b.Nombre) {
+  arreglo.sort(function (fa, fb) {
+	  let a = fa.nombre,
+            b = fb.nombre;
+    if (a > b) {
       return 1;
     }
-    if (a.Nombre < b.Nombre) {
+    if (a < b) {
       return -1;
     }
     // a must be equal to b
