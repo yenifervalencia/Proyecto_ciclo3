79ad8c742006440e349789a8b885f5614b64ad90
JulioAnduckia <julioanduckia@yahoo.es>
2021-10-07 13:07:49 -0500

    hola a todos menos valentina

--- registro.js
+++ registro.js
@@ -1,77 +1,62 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("form-registro").addEventListener('submit',filtrarCorreo)
-});
+/*document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("form-registro").addEventListener('submit',filtrarCorreo)
+});*/
+//comentar lo de arriba
 
 
 let registros = []; 
 
 function agregarRegistro(){
-    //console.log("Capturado");
-    function Persona(
-      nombreP,
-     
-      telefonoP,
-      direccionP,
-      correoP,
-      contrasenaP
-    ) {
-      this.nombreP = nombreP;
-      
-      this.telefonoP = telefonoP;
-      this.direccionP = direccionP;
-      this.correoP = correoP;
-      this.contrasenaP = contrasenaP;
-    }
-    var nombre= document.getElementById("nombre").value;
-    //console.log(nombreCapturar);
-   
-    //console.log(generoCapturar);
-    var telefono = document.getElementById("telefono").value;
-    //console.log(telefonoCapturar);
-    var direccion = document.getElementById("direccion").value;
-    //console.log(direccionCapturar);
-    var correo = document.getElementById("correo").value;
-    //console.log(correoCapturar);
-    var contrasena = document.getElementById("contrasena").value;
-    //console.log(contrasenaCapturar);
-    nuevoCliente = new Persona(
-      nombre,    
-      telefono,
-      direccion,
-      correo,
-      contrasena
-    );
-    //console.log(nuevoCliente);
-   
-  
-  
-      registros.push(nuevoCliente);
-      registros.forEach((e) => {
-        console.log(e);
-    });
-      
+ 
+ let nombreUsu= document.getElementById("nombre").value;
+  let telefonoUsu = document.getElementById("telefono").value;
+  let direccionUsu = document.getElementById("direccion").value;
+  let correoUsu = document.getElementById("correo").value;
+  let contrasenaUsu = document.getElementById("contrasena").value;
+
+  let nuevoCliente = {
+    "nombre": nombreUsu,
+      //    "genero": generoUsu,
+      "telefono": telefonoUsu,
+      "direccion": direccionUsu,
+      "correo": correoUsu,
+      "password": contrasenaUsu
   }
+ 
+
+
+    registros.push(nuevoCliente);
+    registros.forEach((e) => {
+      console.log(e);
+  });
+    
+}
 
 
 function filtrarCorreo(registros){
-    let filtro=registros.filter((arregloo) => arregloo.correo.includes("@gmail.com"));
-	filtro.forEach(element => {
-        console.log(element);
-    });
-	return filtro;      
+  let filtro=registros.filter((arregloo) => arregloo.correo.includes("@gmail.com"));
+filtro.forEach(element => {
+      console.log(element);
+  });
+return filtro;      
 }
 
 function ordenarArreglo(arreglo){
-  arreglo.sort(function (a, b) {
-    if (a.Nombre > b.Nombre) {
-      return 1;
-    }
-    if (a.Nombre < b.Nombre) {
-      return -1;
-    }
-    // a must be equal to b
-    return 0;
-  });
-  console.log(arreglo);
-  return arreglo;
+arreglo.sort(function (fa, fb) {
+  let a = fa.nombre,
+          b = fb.nombre;
+  if (a > b) {
+    return 1;
   }
+  if (a < b) {
+    return -1;
+  }
+  // a must be equal to b
+  return 0;
+});
+console.log(arreglo);
+return arreglo;
+}
+
+module.exports = { agregarRegistro, filtrarCorreo, ordenarArreglo, registros };
+//descomentar el module para subir al bot
